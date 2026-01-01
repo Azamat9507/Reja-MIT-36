9e6e3a448583119364d8d22a23324746296325e4
Azamat9507 <solijonov.azamat9507@gmail.com>
2026-01-02 01:57:08 +0900

    feat: Frontend javascriptni qurish

--- app.js
+++ app.js
@@ -23,20 +23,16 @@ app.post("/create-item", function(req, res) {
     console.log("user entered /create-item");
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-        if(err){
-            console.log(err);
-            res.end('something went wrong');
-        } else {
-            res.end("successfully added");
-        }
-    })
+        console.log(data.ops);
+        res.json(data.ops[0]);
+    });
 
     
 })
 
 
 app.get("/", function (req, res) {
-   console.log("user entered /");
+   console.log("user entered 2/");
     db.collection("plans")
     .find()
     .toArray((err, data) => {
