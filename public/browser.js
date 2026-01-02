06526745ca65e8273f911aa0bc2cb67e42fa76c4
Azamat9507 <solijonov.azamat9507@gmail.com>
2026-01-02 21:06:00 +0900

    fix: CRUD delete vs update operation edits

--- public/browser.js
+++ public/browser.js
@@ -1,59 +1,65 @@
-console.log("frontend ishga tushdi");
 
+console.log("Fronted js ishga tushdi");
 
 function itemTemplate(item) {
-  return `<li 
-    class="list-group-item list-group-item-info d-flex align-items-center justify-content-between">
-    <span class="item-text">${item.reja}</span>
-    <div>
-        <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">
-            O'zgartirish
-        </button>
-        <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm mr-1">O'chirish</button>
-    </div>
-</li>`;
+    return `<li
+    class="list-group-item list-group-item-info d-flex align-items-center justify-content justify-content-between">
+<span class="item-text">${item.reja}</span>
+<div>
+   <button data-id="${item._id}"
+     class="edit-me btn btn-secondary btn-sm mr-1" style="border-radius: 20px">
+       Change
+   </button>
+   <button data-id="${item._id}" id="clean-all" class="btn btn-danger" style="border-radius: 20px">
+       Clear all
+   </button>
+</div>
+</li>`
 }
-
 let createField = document.getElementById("create-field");
 
-document.getElementById("create-form").addEventListener("submit", function (e) {
-  e.preventDefault();
+document
+.getElementById("create-form")
+.addEventListener("submit", function(e) {
+    e.preventDefault();
 
-  axios
+
+    axios
     .post("/create-item", { reja: createField.value })
     .then((response) => {
-      document
+        document
         .getElementById("item-list")
         .insertAdjacentHTML("beforeend", itemTemplate(response.data));
         createField.value = "";
         createField.focus();
     })
     .catch((err) => {
-      console.log("Iltimos qaytadan harakat qiling!");
+        console.log("Iltimos qaytadan harakat qiling!");
     });
-  });
-
- document.addEventListener("click", function (e) {
-  // DELETE oper
-  if (e.target.classList.contains("delete-me")) {
-    if (confirm("Aniq ochirmoqchimisiz?")) {
-      axios
-        .post("/delete-item", {
-          id: e.target.getAttribute("data-id"),
-        })
-        .then((response) => {
-          console.log(response.data);
-          // li elementni o‘chirish
-          e.target.parentElement.parentElement.remove();
-        })
-        .catch((err) => {
-          console.log("Iltimos qaytadan harakat qiling!");
-        });
-    }
-  }
-
-  // EDIT oper (keyin yoziladi)
-  if (e.target.classList.contains("edit-me")) {
-    // edit logikasi shu yerda bo‘ladi
-  }
 });
+document.addEventListener("click", function (e) {
+    // delete operation
+    if (e.target.classList.contains("delete-me")) {
+        if (confirm("Aniq ochirmoqchimisiz?")) {
+         axios
+            .post("/delete-item", { id: e.target.getAttribute("data-id") })
+            .then((respose) => {
+                console.log(respose.data);
+                e.target.parentElement.parentElement.remove();
+            })
+            .catch((err) => {
+                console.log("Iltimos qaytadan harakat qiling");
+            });
+        }
+    }
+
+
+// edit operation
+
+// Delete all operation
+document.getElementById("clean-all").addEventListener("click", function () {
+    axios.post("delete-all", {delete_all: true }).then(respose => {
+        alert(respose.data.state);
+        document.location.reload();
+    })
+})
