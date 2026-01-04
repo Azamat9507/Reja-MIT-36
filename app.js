82dafd6458ed726b47548bc3673b5ac87d0ac4a0
Azamat9507 <solijonov.azamat9507@gmail.com>
2026-01-04 20:11:31 +0900

    fix: Edit operator is done

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
-console.log("Web Serverni boshlash");
+console.log("web serverni boshlash");
 const express = require("express");
 const app = express();
 
 
 
-// MongoDB chaqirish
-const db = require("./server").db();
-const mongoDb = require("mongodb");
+// MongoDB connect
+const db = require("./server").db(); 
+const mongodb = require("mongodb");
 
-// 1
+
+// Kirish code
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// 2: Session code
-// 3 Views code
+// 2: Session
 
-app.set('views', 'views');
+// 3: Views code
+app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
 app.post("/create-item", (req, res) => {
     console.log("user entered /create-item");
     const new_reja = req.body.reja;
-    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+    db.collection("plans").insertOne({reja: new_reja }, (err, data) => {
         console.log(data.ops);
-        res.json(data.ops[0]);
-    });   
-});
-//Delete qilish mantiqi
-app.post("/delete-item", (req, res) => {
-    const id = req.body.id;
-    console.log(id);
-    db.collection("plans").deleteOne({_id: new mongoDb.ObjectId(id)}, function(err, data) {
-        res.json({state: "success"});
+       res.json(data.ops[0]);
     });
 });
-
+// O'chirish amali
+app.post("/delete-item", (req, res) => {
+    const id = req.body.id;
+    db.collection("plans").deleteOne(
+        {_id: new mongodb.ObjectId(id) },
+         function (err, data) {
+            res.json({state: "success"});
+        }
+    );
+});
+// Edit qilish amali
 app.post("/edit-item", (req, res) => {
     const data = req.body;
     console.log(data);
@@ -48,7 +51,7 @@ app.post("/edit-item", (req, res) => {
         }
     );
 });
-
+// Hammasini ochirish 
 app.post("/delete-all", (req, res) => {
     if(req.body.delete_all) {
         db.collection("plans").deleteMany(function() {
@@ -57,26 +60,21 @@ app.post("/delete-all", (req, res) => {
     }
 });
 
-
 app.get("/", function (req, res) {
-   console.log("user entered 2/");
+    console.log("user entered /");
     db.collection("plans")
     .find()
     .toArray((err, data) => {
         if(err) {
             console.log(err);
             res.end("something went wrong");
-        } else{
-            
-            res.render("reja", {items: data});
+        } else {
+            console.log(data);
+            res.render("reja", {items: data });
         }
     });
+    
 });
-
-app.get("/author", function(req, res) {
-    res.render("author", {user: user} );
-})
-
 
 
 module.exports = app;
